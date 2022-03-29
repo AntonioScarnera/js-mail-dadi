d52167fae0cd8a2e43e36012ff806eba85f3f944
AntonioScarnera <scarnerantonio07@gmail.com>
2022-03-29 15:58:56 +0200

    ese

---
+++ script.js
@@ -0,0 +1,55 @@
+//email con controllo
+
+let userEmail = document.getElementById('email');
+
+let submit = document.getElementById('submit');
+
+let mailInfo = document.getElementById('mailInfo');
+console.log(userEmail, submit ,mailInfo);
+
+submit.addEventListener('click', function(){
+    let email = userEmail.value;
+    if(email=='')
+    {
+        alert("Devi indicare un indirizzo email"); 
+        return false;
+    }
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    {
+       alert("L'indirizzo email che hai inserito e' valido")
+       mailInfo.innerHTML = `E-mail: ${email}`;
+       mailInfo.classList.remove("d-none");
+    }
+    else {
+       alert("L'indirizzo email che hai inserito non e' valido");
+       mailInfo.innerHTML = `E-mail non valida: ${email}`;
+       mailInfo.classList.remove("d-none");
+    }
+})
+
+//gioco dei dadi contro il PC
+
+let dadi = document.getElementById('dadi');
+let play = document.getElementById('play');
+let result = document.getElementById('result')
+
+
+play.addEventListener('click', function(){
+    let computer = Math.floor(Math.random() * 6);
+    let player = Math.floor(Math.random() * 6);
+    console.log(computer,player)
+    if(computer>player)
+    {
+        dadi.innerHTML= 'PC: '+computer+ ' - ' +player+' :Player';        result.innerHTML= 'Bella partita! Ritenta, sarai pi&ugrave fortunato...';
+
+    }else if(computer<player){
+        dadi.innerHTML= 'PC: '+computer+ ' - ' +player+' :Player';
+        result.innerHTML= 'Che fortuna! Mi hai battuto!';
+
+    }else{
+        dadi.innerHTML= 'PC: '+computer+ ' - ' +player+' :Player';        result.innerHTML= 'Ops, abbiamo pareggiato...';
+        
+    };
+});
+
+
