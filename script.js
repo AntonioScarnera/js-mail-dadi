0cf099523d222a47ccdce2b50b66d917e1e92b9d
AntonioScarnera <scarnerantonio07@gmail.com>
2022-03-30 10:37:41 +0200

    correzzione esercizio

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //email con controllo
 
+const listaEmail = ["helloworld@gmail.com", "helloworld@icloud.com","helloworld@live.it", "helloworld@outlook.it"];
+let coincide = false;
+
 let userEmail = document.getElementById('email');
 
 let submit = document.getElementById('submit');
@@ -9,16 +12,33 @@ console.log(userEmail, submit ,mailInfo);
 
 submit.addEventListener('click', function(){
     let email = userEmail.value;
+    let controllore = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(email=='')
     {
         alert("Devi indicare un indirizzo email"); 
         return false;
     }
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    if (controllore.test(email))
     {
-       alert("L'indirizzo email che hai inserito e' valido")
-       mailInfo.innerHTML = `E-mail: ${email}`;
-       mailInfo.classList.remove("d-none");
+        for(let i = 0; i < listaEmail.length; i++){
+
+            if(listaEmail.includes(email)){
+                coincide = true;
+            }else{
+                coincide = false;
+            }
+        }
+        if(coincide){
+            alert("L'indirizzo email che hai inserito e' valido")
+            mailInfo.innerHTML = `E-mail: ${email}`;
+            mailInfo.classList.remove("d-none");
+        }else{
+            alert("L'indirizzo email che hai inserito e' formattata correttamente ma non è valida")
+            mailInfo.innerHTML = `E-mail: ${email}`;
+            mailInfo.classList.remove("d-none");
+        }
+
+
     }
     else {
        alert("L'indirizzo email che hai inserito non e' valido");
